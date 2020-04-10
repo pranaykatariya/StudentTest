38583e17d7ac7993a107bb70e44dc037b59b21ea
Your Name <pranaykatariya1@gmail.com>
2020-04-10 15:39:00 +0530

    first commit

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -14,48 +14,7 @@ export class ResultComponent implements OnInit {
   {
     
   }
-  // ngOnInit() {
-    
-  //   this.PieChart = new Chart('pieChart',{
-  //   type:'pie',
-  //   data:{
-  //     labels:["blue","Green","Pink"],
-  //     datasets:[{
- 
-  //      label:'Vote Now',
-  //      data:[101,102,103],
-  //      backgroundColor:[
- 
-  //        'rgba(40,23,244,0.9)',
-  //        'rgba(192,255,0,0.9)',
-  //        'rgba(239,23,240,0.9)'
- 
-  //      ],
- 
-  //     }]
-  //   },
-  //   options:{
- 
-  //    title:{
-  //      //Text:"Bar chart",
-  //      display:true
-  //    },
- 
-  //    scales:{
- 
-  //      yAxes:[{
-  //        ticks:{
-  //          beginAtZero:true
-  //        }
-  //      }]
-  //    }
- 
- 
-  //   }
-  // });
- 
- 
-  //}
+  
 
 }
 
