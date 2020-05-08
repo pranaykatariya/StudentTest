582cc38f16ec337e5e020e60ab63767ce870a1ab
Rida <kaziridzz@gmail.com>
2020-05-09 02:05:33 +0530

    admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { StudentService } from '../services/student.service';
 import { CommonURLService } from '../services/common-url.service';
+import { StudentMark } from 'src/models/StudentMark.model';
 
 @Component({
   selector: 'app-admin',
@@ -10,19 +11,7 @@ import { CommonURLService } from '../services/common-url.service';
 })
 export class AdminComponent implements OnInit {
 
-    private fname:string;
-    private lname:string;
-    private email: string;
-    private mob:string;
-    private gender:string;
-    private apti:number;
-    private comm:number;
-    private tech:number;
-    private wri:number;
-    private hscp:number;
-    private hscm:number;
-    private sscp:number;
-    private sscm:number;
+    private data:StudentMark[];
 
 
   constructor(private router: Router, private studentService: StudentService) { }
@@ -42,19 +31,7 @@ export class AdminComponent implements OnInit {
     setTimeout(() => {
       console.log(this.studentService.studentsData);
 
-      this.fname=this.studentService.studentsData[0].firstName;
-      this.lname=this.studentService.studentsData[0].lastName;
-      this.email=this.studentService.studentsData[0].email;
-      this.mob=this.studentService.studentsData[0].mobile;
-      this.gender=this.studentService.studentsData[0].gender;
-      this.apti=this.studentService.studentsData[0].aptitude_marks;
-      this.comm=this.studentService.studentsData[0].communication_marks;
-      this.tech=this.studentService.studentsData[0].technical_marks;
-      this.wri=this.studentService.studentsData[0].writing_marks;
-      this.hscp=this.studentService.studentsData[0].HSC_Percentage;
-      this.hscm=this.studentService.studentsData[0].HSC_Maths;
-      this.sscp=this.studentService.studentsData[0].SSC_Percentage;
-      this.sscm=this.studentService.studentsData[0].SSC_Maths;
+      this.data=this.studentService.studentsData;
 
     }, 3000);
   }
