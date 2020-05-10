e53d16037d7924c7b87cc8144bcd10be8d91a5ba
pranaykatariya <pranaykatariya1@gmail.com>
2020-05-11 00:47:44 +0530

    result component modified

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -16,7 +16,7 @@ export class ResultComponent implements OnInit {
 
  
   private email: string;
-
+  private activeUserUrl: string;
   
   
   PieChart=[];
@@ -26,24 +26,27 @@ export class ResultComponent implements OnInit {
   
   ngOnInit()
   {
-    
+    this.email = sessionStorage.getItem('email')
 
-    this.studentService.getAllStudents();
+    this.activeUserUrl = "https://restpsp.herokuapp.com/students/"+this.email;
+    this.studentService.getStudent(this.activeUserUrl);
 
     //You have to work on data stored in this variable
-    this.studentService.studentsData
+    this.studentService.activeUser
+
+    
 
     //this is all students data in json format available after 3 sec 
     setTimeout(() => {
-      console.log(this.studentService.studentsData);
-    
+      console.log(this.studentService.activeUser)     
+      // this.studentService.activeUser.communication_marks
     
       // this.communication_marks=this.studentService.studentsData.communication_marks;
       // this.technical_marks=this.studentService.studentsData[0].technical_marks;
       // this.writing_marks=this.studentService.studentsData[0].writing_marks;
       // this.aptitude_marks=this.studentService.studentsData[0].aptitude_marks;
       
-       
+      
 
     }, 3000);
 
