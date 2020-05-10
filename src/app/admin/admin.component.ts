e3bc8e2c370ce377458b2ee60396bbad8a693566
Rida <kaziridzz@gmail.com>
2020-05-10 23:41:26 +0530

    changes

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { StudentService } from '../services/student.service';
-import { CommonURLService } from '../services/common-url.service';
 import { StudentMark } from 'src/models/StudentMark.model';
+
 
 @Component({
   selector: 'app-admin',
@@ -12,18 +12,29 @@ import { StudentMark } from 'src/models/StudentMark.model';
 export class AdminComponent implements OnInit {
 
     private data:StudentMark[];
+    private length: number;
+    private avg:number;
+    private sum:number;
+    
 
 
-  constructor(private router: Router, private studentService: StudentService) { }
+
+  constructor(private router: Router, private studentService: StudentService) { 
+
+
+  }
 
   ngOnInit() {
 
     if( sessionStorage.getItem('admin') ){
         console.log("Authorized user")
+
     }else{
       this.router.navigate([
       '/admin-login'
-    ])  
+    ])
+    
+    
     }
 
     if(this.studentService.studentsData.length > 0)
@@ -33,6 +44,7 @@ export class AdminComponent implements OnInit {
       console.log(this.studentService.studentsData);
 
       this.data=this.studentService.studentsData;
+
 
     }, 30);
     }
@@ -45,10 +57,24 @@ export class AdminComponent implements OnInit {
         console.log(this.studentService.studentsData);
   
         this.data=this.studentService.studentsData;
-  
+      
+  /*    
+        function average() {
+          this.sum=0;
+        for(var i=0;i<this.data.length;i++)
+        {
+          this.sum += this.data[i].aptitude_marks;
+        }
+        this.avg=this.sum/this.data.length;
+          return this.avg;
+        }
+*/
       }, 18000);
 
     }
+
+    
+
   }
 
 }
